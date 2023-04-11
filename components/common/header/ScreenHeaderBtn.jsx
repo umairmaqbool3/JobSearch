1ec9118bbbb93e95d07ff4723db0fd2cc7506c74
Umair Maqbool <20332318+umairmaqbool3@users.noreply.github.com>
2023-04-11 23:00:17 +0500

    Update ScreenHeaderBtn.jsx

--- components/common/header/ScreenHeaderBtn.jsx
+++ components/common/header/ScreenHeaderBtn.jsx
@@ -6,6 +6,7 @@ import styles from './screenheader.style'
 const ScreenHeaderBtn = ({iconUrl, dimension, handlePress}) => {
   return (
     <TouchableOpacity style={styles.btnContainer} onPress={handlePress}>
+//     <Text></Text>
       <Image source={iconUrl}
         resizeMode="cover"
         style={styles.btnImg(dimension)}
