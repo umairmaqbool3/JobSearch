8cb2122d30986af8f323b9bc71213ed8c84e8751
Umair Maqbool <20332318+umairmaqbool3@users.noreply.github.com>
2023-05-22 22:29:42 +0500

    Update useFetch.js

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -6,7 +6,7 @@ const useFetch = (endpoint, query) => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  // api call
+  // main api call
   const options = {
     method: "GET",
     url: `https://jsearch.p.rapidapi.com/${endpoint}`,
